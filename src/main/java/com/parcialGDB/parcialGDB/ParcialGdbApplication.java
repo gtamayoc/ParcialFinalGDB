5f1717abe2eaa1983a195cb8a0b25ce596c201a9
Giuseppe Tamayo <gtamayoc@ufpso.edu.co>
2023-06-19 12:36:53 -0500

    IMPLEMENTACIÒN - Archivos base para API en springboot

---
+++ src/main/java/com/parcialGDB/parcialGDB/ParcialGdbApplication.java
@@ -0,0 +1,13 @@
+package com.parcialGDB.parcialGDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialGdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialGdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parcialGDB/parcialGDB/ParcialGdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcialGDB.parcialGDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialGdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
