44f6f035cdc20cdf1e9f1685afc5a49ee718daae
Giuseppe Tamayo <gtamayoc@ufpso.edu.co>
2023-06-20 03:15:56 -0500

    IMPLEMENTACIÒN - Se agrega view de login

---
+++ Android/app/src/main/java/com/example/parcialfinalgdb/Vista/Main.java
@@ -0,0 +1,16 @@
+package com.example.parcialfinalgdb.Vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.parcialfinalgdb.R;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
